ce3fd9c9e08b2758482c7837f80b7db2c2e64ea3
RemKuzo <rikuzo36@gmail.com>
2020-12-31 00:29:20 +0200

    initial commit

--- src/controllers/index.js
+++
@@ -1,3 +0,0 @@
-exports.getWelcomeController = (req, res) => {
-  res.status(200).json({ message: 'Welcome to the ToDo App where your task lists are well organized' });
-};
--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import welcomeController from '../controllers';
-
-const router = express.Router();
-router.get('/', welcomeController.getWelcomeController);
-export default router;
